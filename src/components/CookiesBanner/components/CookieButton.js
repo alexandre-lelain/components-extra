a42ea97343015cabb8c08696ed3f3dc6fbb305f1
Alexandre Le Lain <lelain.alexandre@gmail.com>
2019-10-02 14:33:49 +0200

    feat(cookies-banner): Add CookiesBanner v1.0

--- src/components/CookiesBanner/components/CookieButton.js
+++ src/components/CookiesBanner/components/CookieButton.js
@@ -16,7 +16,7 @@ const CookieButton = ({ className, href = undefined, label = '', onClick = undef
   return (
     <Button
       className={`${className} ${classes.label}`}
-      variant="contained"
+      variant="outlined"
       color="secondary"
       href={href}
       onClick={onClick}
