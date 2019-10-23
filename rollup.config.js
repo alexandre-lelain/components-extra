13ee62aa9451418bcef970180a762125d4403d92
Alexandre Le Lain <lelain.alexandre@gmail.com>
2019-10-23 17:55:48 +0200

    Feature: Add rollup to enable tree-shaking (#6)
    * feat(rollup): initialized rollup
    
    * feat(rollup): added esm build
    
    * chore(rollup): removed old webpack config
    
    * build(rollup, babel): make the lib tree-shakable and add JS builds support
    
    * ci(scripts, travis): prevent lib to build at install
    
    * ci(travis): use only one job to faster the pipeline

---
+++ rollup.config.js
@@ -0,0 +1,54 @@
+import path from 'path'
+
+import alias from 'rollup-plugin-alias'
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import replace from 'rollup-plugin-replace'
+import resolve from 'rollup-plugin-node-resolve'
+import { sizeSnapshot } from "rollup-plugin-size-snapshot"
+import { terser } from 'rollup-plugin-terser'
+
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'styled-components': 'styled',
+}
+
+const plugins = [
+  alias({
+    resolve: ['.js', '/index.js'],
+    entries: [
+      { find: 'theme', replacement: path.resolve(__dirname, './src/theme') },
+      { find: 'utils', replacement: path.resolve(__dirname, './src/utils') },
+    ],
+  }),
+  babel({ exclude: /node_modules/, runtimeHelpers: true }),
+  commonjs({ 
+    include: /node_modules/,
+    namedExports: {
+      'react-is': ['ForwardRef'],
+      'prop-types': ['elementType'],
+    }
+  }),
+  peerDepsExternal(),
+  replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+  resolve({ 
+    extensions: ['.js'],
+    preferBuiltins: false 
+  }),
+  sizeSnapshot(),
+  terser(),
+]
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'build/umd/components-extra.min.js',
+    format: 'umd',
+    name: 'components-extra',
+    esModule: false,
+    globals,
+  },
+  plugins,
+}
