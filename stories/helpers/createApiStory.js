7ab03873947fb8e84710b26c20cd1356cfb938f6
Alexandre Le Lain <lelain.alexandre@gmail.com>
2019-11-27 15:27:30 +0100

    docs(credit-card-number): added new story

--- stories/helpers/createApiStory.js
+++ stories/helpers/createApiStory.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default (story, Component) => {
+export default (story, Component, info) => {
   story.add('API', () => <div />, {
-    info: { inline: true, source: false, propTables: [Component] },
+    info: { inline: true, source: false, propTables: [Component], text: info },
     options: { showPanel: false },
   })
 }
