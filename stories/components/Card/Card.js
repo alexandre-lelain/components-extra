8e4eef80bcc064eff446c53a614b84f7e152c835
Alexandre Le Lain <lelain.alexandre@gmail.com>
2019-11-27 11:40:07 +0100

    feat(Card): Card is now following compound pattern

---
+++ stories/components/Card/Card.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import styled from 'styled-components'
+
+import Card, { BaseCard } from 'components/Card'
+
+import createApiStory from '../../helpers/createApiStory'
+
+const StyledCard = styled(Card)`
+  width: 70%;
+  margin: auto;
+  text-align: center;
+`
+StyledCard.displayName = 'Card'
+
+const story = storiesOf('Components|Card', module)
+
+story.add('Default', () => (
+  <StyledCard
+    description="This is an awesome card. An awesome card is easy to use and only have a few props so you can implement it very quickly, while you can still customize its colors. Did you know this component is built on top of material-ui's Card ?"
+    image={{ src: '//torange.biz/photofx/74/8/coffee-cup-drop-74081.jpg' }}
+    onClick={action('Awesome card clicked!')}
+    title="Awesome card"
+  >
+    <Card.Button onClick={action('Awesome button clicked!')}>Awesome button</Card.Button>
+    <Card.Button href="#">Awesome link</Card.Button>
+  </StyledCard>
+))
+
+story.add('Without media', () => (
+  <StyledCard
+    description="This is an awesome card. An awesome card is easy to use and only have a few props so you can implement it very quickly, while you can still customize its colors. Did you know this component is built on top of material-ui's Card ?"
+    onClick={action('Awesome card clicked!')}
+    title="Awesome card"
+  >
+    <Card.Button onClick={action('Awesome button clicked!')}>Awesome button</Card.Button>
+    <Card.Button href="#">Awesome link</Card.Button>
+    <Card.Button href="#">Awesome link the return</Card.Button>
+  </StyledCard>
+))
+
+story.add('Without buttons', () => (
+  <StyledCard
+    description="This is an awesome card. An awesome card is easy to use and only have a few props so you can implement it very quickly, while you can still customize its colors. Did you know this component is built on top of material-ui's Card ?"
+    image={{ src: '//torange.biz/photofx/74/8/coffee-cup-drop-74081.jpg' }}
+    onClick={action('Awesome card clicked!')}
+    title="Awesome card"
+  />
+))
+
+story.add('With your own custom button', () => (
+  <StyledCard
+    description="This is an awesome card. An awesome card is easy to use and only have a few props so you can implement it very quickly, while you can still customize its colors. Did you know this component is built on top of material-ui's Card ?"
+    image={{ src: '//torange.biz/photofx/74/8/coffee-cup-drop-74081.jpg' }}
+    onClick={action('Awesome card clicked!')}
+    title="Awesome card"
+  >
+    <button onClick={action('clicked on your custom constrol component')}>
+      This is your component
+    </button>
+  </StyledCard>
+))
+
+createApiStory(story, BaseCard)
