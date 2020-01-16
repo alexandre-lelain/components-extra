833c346a60f2d5e0a8550ba5b4715d189f9e68fb
Alexandre Le Lain <lelain.alexandre@gmail.com>
2020-01-17 00:37:56 +0100

    chore(deps, jest): removed obsolete deps & added jest config

--- .storybook/components/helpers/propsFilter.js
+++
@@ -1,6 +0,0 @@
-const EXCLUDED = ['forwardedRef', 'ref']
-
-const filterProps = (propsDefinition = []) =>
-  propsDefinition.filter(prop => !EXCLUDED.includes(prop.property))
-
-export default filterProps
