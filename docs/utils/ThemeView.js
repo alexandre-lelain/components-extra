1651f748611f8ec9aacfda20c1189fca9c30a50a
Alexandre Le Lain <lelain.alexandre@gmail.com>
2020-01-17 22:19:33 +0100

    docs(customization): added customization part

---
+++ docs/utils/ThemeView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import theme from 'theme'
+
+export default () => {
+  if (typeof window !== "undefined") {
+   const ReactJson = require('react-json-view').default
+   return <ReactJson style={{ fontSize: 16 }} name={null} theme="ocean" src={theme} sortKeys displayDataTypes={false} />
+  }
+  return null
+}
