8e8216421ab8d2ade15c19b34b9f05f10e25c308
Alexandre Le Lain <lelain.alexandre@gmail.com>
2020-01-30 11:18:04 +0100

    docs(card): added all card stories

--- docs/docs-utils/Card.js
+++ docs/docs-utils/Card.js
@@ -6,3 +6,23 @@ export const Card = styled(RealCard)`
   width: 70%;
   margin: auto;
 `
+
+export const ExtendedCard = styled(RealCard)`
+  width: 90%;
+  margin: auto;
+  background-color: green;
+  border-radius: 18px;
+  box-shadow: 2px 2px 2px 2px grey;
+  * {
+    color: white;
+  }
+`
+
+export const ExtendedCardButton = styled(RealCard.Button)`
+  background-color: green;
+  border-radius: 18px;
+  box-shadow: 2px 2px 2px 2px grey;
+  * {
+    color: white;
+  }
+`
