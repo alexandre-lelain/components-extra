c35d168530a10f18a91497575fa1211c31ea06b2
Alexandre Le Lain <lelain.alexandre@gmail.com>
2020-02-02 13:37:03 +0100

    chore(prettier): run prettier on docs/ js files

--- docs/docs-utils/Props.js
+++ docs/docs-utils/Props.js
@@ -3,7 +3,9 @@ import { Props } from 'docz'
 
 export default ({ children, ...rest }) => (
   <>
-    <p>The <code>ref</code> will be forwarded to the html root element, as well as any other props.</p>
+    <p>
+      The <code>ref</code> will be forwarded to the html root element, as well as any other props.
+    </p>
     {children}
     <Props {...rest} />
   </>
--- docs/docs-utils/ThemeView.js
+++ docs/docs-utils/ThemeView.js
@@ -3,9 +3,18 @@ import React from 'react'
 import theme from 'theme'
 
 export default () => {
-  if (typeof window !== "undefined") {
-   const ReactJson = require('react-json-view').default
-   return <ReactJson style={{ fontSize: 16 }} name={null} theme="ocean" src={theme} sortKeys displayDataTypes={false} />
+  if (typeof window !== 'undefined') {
+    const ReactJson = require('react-json-view').default
+    return (
+      <ReactJson
+        style={{ fontSize: 16 }}
+        name={null}
+        theme="ocean"
+        src={theme}
+        sortKeys
+        displayDataTypes={false}
+      />
+    )
   }
   return null
 }
