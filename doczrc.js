58df74932df3b9baa386c54f0f3da9fb663ec3b8
Alexandre Le Lain <lelain.alexandre@gmail.com>
2020-02-02 14:08:28 +0100

    docs(build): fixed wrong build config

--- doczrc.js
+++ doczrc.js
@@ -1,21 +1,10 @@
-let config = {}
-/*
- * Need to wrap in a try/catch as Docz is trying to require the package before 
- * being in .docz repo.
- */
-try {
-  const packageInfo = require('../package.json')
-
-  config = {
-    title: 'Components-extra - v'+packageInfo.version,
-    description: 'React UI molecule components based on styled-components & material-ui.',
-    menu: ['Introduction', 'Components', 'Utils', 'Customization', 'Examples', 'FAQ'],
-    ignore: ['README.md', 'CHANGELOG.md', 'LICENSE', 'build/**', 'build', 'node_modules'],
-    dest: 'docs-build',
-    themeConfig: {
-      showPlaygroundEditor: false,
-    }
+export default {
+  title: 'Components-extra',
+  description: 'React UI molecule components based on styled-components & material-ui.',
+  menu: ['Introduction', 'Components', 'Utils', 'Customization', 'Examples', 'FAQ'],
+  ignore: ['README.md', 'CHANGELOG.md', 'LICENSE', 'build/**', 'build', 'node_modules'],
+  dest: 'docs-build',
+  themeConfig: {
+    showPlaygroundEditor: false,
   }
-} catch {}
-
-export default config
+}
