84c0e94381b576ba3402a4b2f806883dad793452
Alexandre Le Lain <lelain.alexandre@gmail.com>
2020-02-02 13:51:37 +0100

    chore(deps): use lts versions

--- doczrc.js
+++ doczrc.js
@@ -1,12 +1,21 @@
-import packageInfo from '../package.json'
+let config = {}
+/*
+ * Need to wrap in a try/catch as Docz is trying to require the package before 
+ * being in .docz repo.
+ */
+try {
+  const packageInfo = require('../package.json')
 
-export default {
-  title: 'Components-extra - v'+packageInfo.version,
-  description: 'React UI molecule components based on styled-components & material-ui.',
-  menu: ['Introduction', 'Components', 'Utils', 'Customization', 'Examples', 'FAQ'],
-  ignore: ['README.md', 'CHANGELOG.md', 'LICENSE', 'build/**', 'build', 'node_modules'],
-  dest: 'docs-build',
-  themeConfig: {
-    showPlaygroundEditor: false,
+  config = {
+    title: 'Components-extra - v'+packageInfo.version,
+    description: 'React UI molecule components based on styled-components & material-ui.',
+    menu: ['Introduction', 'Components', 'Utils', 'Customization', 'Examples', 'FAQ'],
+    ignore: ['README.md', 'CHANGELOG.md', 'LICENSE', 'build/**', 'build', 'node_modules'],
+    dest: 'docs-build',
+    themeConfig: {
+      showPlaygroundEditor: false,
+    }
   }
-}
+} catch {}
+
+export default config
